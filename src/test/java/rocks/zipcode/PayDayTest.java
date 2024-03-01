8d684b7718165db1ba5816f7b99ee7919276f047
tb0902 <tbruton417@gmail.com>
2024-03-01 12:13:42 -0500

    Final commit

--- src/test/java/rocks/zipcode/PayDayTest.java
+++ src/test/java/rocks/zipcode/PayDayTest.java
@@ -13,7 +13,11 @@ public class PayDayTest {
         assertEquals("Kris 215.00 10.75 204.25", string1);
 
         PayDay payDay2 = new PayDay();
-        java.lang.String string2 =payDay2.pay("Dolio", 23.0, 12.0, 0.05),)
+        java.lang.String string2 =payDay2.pay("Dolio", 23.0, 12.0, 0.05);
+        assertEquals("Dolio 276.00 13.80 262.20", string2);
 
+        PayDay payDay3 = new PayDay();
+        java.lang.String string3 =payDay3.pay("Karen", 22.0, 12.0, 0.05);
+        assertEquals("Karen 264.00 13.20 250.80", string3);
     }
 }
